c73040ffc0610547ddd1a255b07e1342fc7b7db4
SubhamD1999 <subhamdebnath028@gmail.com>
2026-09-15 13:24:07 +0530

    sfds

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata: Metadata = { 
   title: "SCMS Dashboard",
   description: "Dashboard Analytics for SCMS",
 };
